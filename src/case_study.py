Fix m,L calculation for polytope synthesis, Fix heavy_ball beta parameter

src/case_study.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from fct.algorithms import GradientDescent, Nesterov, TMM, Algorithm # To be removed later
 from fct.objectives import PeriodicExample2D, WindowedLeastSquares
-from lib.tracking_analysis import bisection_thm1, bisection_thm2, bisection_thm3
+from lib.tracking_analysis import bisection_thm1, bisection_thm2
 from lib.utils import consistent_polytope_nd, calculate_L_m_bounds, visualize
 from lib.algorithms_unconstrained import gradient_descent, nesterov, heavy_ball, triple_momentum
 from tqdm import tqdm
@@ -87,20 +87,26 @@ def run_simulation(algo_names=['gradient', 'nesterov', 'tmm'], obj=None, x0=None
         n_grid = 2
         grid_step = (L_max - L_min) / n_grid
 
-        params = np.array([np.linspace(L_min, L_max, n_grid + 1),
-            np.linspace(m_min, m_max, n_grid + 1)])
-        # Grid points consisting of p_k and delta_p === p(theta) Δtheta
-        grid_points = consistent_polytope_nd(params, np.array([ delta_L_min, delta_m_min]),
-                                                     np.array([ delta_L_max, delta_m_max]),
-                                                     step_size=grid_step)
+        n_sparse = 4
+        t_min, t_max = 0, np.tanh(1)
+
+        delta_params_min = np.array([delta_m_min, delta_L_min])
+        delta_params_max = np.array([delta_m_max, delta_L_max])
+
+        t = np.linspace(t_min, t_max, n_sparse)
+        m = 2 + t**2 - t * np.sqrt(t**2 + 4)
+        L = 2 + t**2 + t * np.sqrt(t**2 + 4)
+        params = np.vstack((m, L))
+        step_size = ( np.min(delta_params_max) - np.max(delta_params_min) ) / n_sparse
+        grid_points = consistent_polytope_nd(params, delta_params_min, delta_params_max, step_size=step_size)
+
         # visualize(grid_points)
         # print(grid_points)
-
-        # rho_sec, sol_sec = bisection_thm1(algo=algo, consistent_polytope=[(np.array([L_k]), 0)], optimize_bound=True)
 
         rho_sec, sol_sec = bisection_thm1(algo=algo, consistent_polytope=grid_points, optimize_bound=True)
         algorithm.rho_sec = rho_sec
         algorithm.c_sec = sol_sec
+        print(f"Algorithm parameters for {algo_name}: rho_sec, {rho_sec}, c_sec=Cond(P)=, {sol_sec}")
 
         rho, sol = bisection_thm2(algo=algo, consistent_polytope=grid_points, optimize_bound=True)
         algorithm.rho = rho
@@ -108,8 +114,7 @@ def run_simulation(algo_names=['gradient', 'nesterov', 'tmm'], obj=None, x0=None
         algorithm.lambd = sol[1]
         algorithm.gamma_xi = np.asarray(sol[2]).item()
         algorithm.gamma_delta = np.asarray(sol[3]).item()
-
-        print(f"Algorithm parameters for {algo_name}: rho, {algorithm.rho}, c1, {algorithm.c1}, c2, {algorithm.c2}, lambda, {algorithm.lambd}, gamma_xi, {algorithm.gamma_xi}, gamma_delta, {algorithm.gamma_delta}")
+        print(f"Algorithm parameters for {algo_name}: rho, {algorithm.rho}, c1, {algorithm.c1}, c1=cond(P)**2, cond(P)={np.sqrt(algorithm.c1)}, c2, {algorithm.c2}, lambda, {algorithm.lambd}, gamma_xi, {algorithm.gamma_xi}, gamma_delta, {algorithm.gamma_delta}")
 
         # Delta_delta_km1_s_km1 = [] # TODO to be used if p>1
 

src/lib/algorithms_unconstrained.py
@@ -16,10 +16,12 @@ def gradient_descent(m,L):
 
 def heavy_ball(m,L):
     alpha = (2/(np.sqrt(L)+np.sqrt(m)))**2
-    beta = (np.sqrt(L/m)-1) / (np.sqrt(L/m)+1)
+    beta = (np.sqrt(L/m)-1) / (np.sqrt(L/m)+1)**2 # TODO: This should be squared according to Lessard's paper
 
-    A = np.asarray([[1+beta, -beta], [1, 0]])
-    B = np.asarray([[-alpha], [0]])
+    A = np.asarray([[1+beta, -beta],
+                    [1,          0]])
+    B = np.asarray([[-alpha],
+                    [0     ]])
     C = np.asarray([[1, 0]])
     D = 0
 
@@ -32,8 +34,10 @@ def nesterov(m,L):
     alpha = 1/L
     beta = (np.sqrt(L/m)-1) / (np.sqrt(L/m)+1)
 
-    A = np.asarray([[1+beta, -beta], [1, 0]])
-    B = np.asarray([[-alpha], [0]])
+    A = np.asarray([[1+beta, -beta],
+                    [1,          0]])
+    B = np.asarray([[-alpha],
+                    [0     ]])
     C = np.asarray([[1+beta, -beta]])
     D = 0
 

src/lib/tracking_analysis.py
@@ -61,7 +61,7 @@ def bisection_thm1(algo, consistent_polytope, optimize_bound=True, rho_max=1.5, 
             P_kp1 = lyap.P(p_kp1)
 
             ### algorithm ### TODO: update with lambda function m(p_k), L(p_k) instead of m=1, L=p_k
-            m, L = p_k[1], p_k[0]
+            m, L = p_k[0], p_k[1]
             G, p, q = algo(m,L)
 
             G_hat, Psi = build_lure_system(G, m, L, p, q, vIQC=False)
@@ -161,7 +161,7 @@ def calculate_sensitivities_over_rhos_sectional(algo,
             P_kp1 = lyap.P(p_kp1)
 
             ### algorithm ### TODO: update with lambda function m(p_k), L(p_k) instead of m=1, L=p_k
-            m, L = p_k[1], p_k[0]
+            m, L = p_k[0], p_k[1]
             G, p, q = algo(m,L)
 
             G_hat, Psi = build_lure_system(G, m, L, p, q, vIQC=False)
@@ -254,7 +254,7 @@ def bisection_thm1_with_intermediate_rho_sens(algo, consistent_polytope, optimiz
             P_kp1 = lyap.P(p_kp1)
 
             ### algorithm ### TODO: update with lambda function m(p_k), L(p_k) instead of m=1, L=p_k
-            m, L = p_k[1], p_k[0]
+            m, L = p_k[0], p_k[1]
             G, p, q = algo(m,L)
 
             G_hat, Psi = build_lure_system(G, m, L, p, q, vIQC=False)
@@ -356,7 +356,7 @@ def bisection_thm2(algo, consistent_polytope, optimize_bound=False, rho_max=1.5,
             P_kp1 = lyap.P(p_kp1)
 
             ### algorithm ### TODO: update with lambda function m(p_k), L(p_k) instead of m=1, L=p_k
-            m, L = p_k[1], p_k[0]
+            m, L = p_k[0], p_k[1]
             G, p, q = algo(m,L)
             AG, BG, CG, DG = ctrl.ssdata(G)
 
@@ -493,7 +493,7 @@ def bisection_thm2_with_intermediate_rho_sens(algo, consistent_polytope, optimiz
             P_kp1 = lyap.P(p_kp1)
 
             ### algorithm ### TODO: update with lambda function m(p_k), L(p_k) instead of m=1, L=p_k
-            m, L = p_k[1], p_k[0]
+            m, L = p_k[0], p_k[1]
             G, p, q = algo(m,L)
             AG, BG, CG, DG = ctrl.ssdata(G)
 
@@ -601,7 +601,7 @@ def calculate_sensitivities_over_rhos(
     # iterate over rho grid
     rhos = np.arange(rho_lo, rho_hi + 1e-12, rho_step)
 
-    for rho in tqdm(rhos, desc="Thm5 bisection over rho"):
+    for rho in tqdm(rhos, desc="Calc sensitivities over rho"):
         entry = {
             'rho': float(np.round(rho, 4)),
             # 'feasible': False,
@@ -632,7 +632,7 @@ def calculate_sensitivities_over_rhos(
             P_kp1 = lyap.P(p_kp1)
 
             ### algorithm ### TODO: update with lambda function m(p_k), L(p_k) instead of m=1, L=p_k
-            m, L = p_k[1], p_k[0]
+            m, L = p_k[0], p_k[1]
             G, p, q = algo(m, L)
             AG, BG, CG, DG = ctrl.ssdata(G)
 